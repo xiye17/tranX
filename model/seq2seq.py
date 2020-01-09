2c7987f76d4e0ca02df0245ef448ba30c676bce3
Xi <xiye17@gmail.com>
2020-01-09 16:06:35 -0600

    fix warning

--- model/seq2seq.py
+++ model/seq2seq.py
@@ -221,7 +221,7 @@ class Seq2SeqModel(nn.Module):
         # (batch_size, src_sent_len)
         att_weight = torch.bmm(src_encoding_att_linear, h_t.unsqueeze(2)).squeeze(2)
         if mask is not None:
-            att_weight.data.masked_fill_(mask, -float('inf'))
+            att_weight.data.masked_fill_(mask.bool(), -float('inf'))
         att_weight = F.softmax(att_weight, dim=-1)
 
         att_view = (att_weight.size(0), 1, att_weight.size(1))
